4d75214fa796cc2b557fe3e93836a6b2be727794
Jeferson Santos <jeferson_jb@live.com>
2025-05-01 10:00:54 -0300

    fix: imports errors

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { Container, Logo, Avatar } from "./styles";
 
-import logoImg from "../../assets/logo.png";
-import avatarImg from "../../assets/avatar.png";
-
 export function Header() {
   return (
     <Container>
-      <Logo source={logoImg} />
-      <Avatar source={avatarImg} />
+      <Logo source="https://placehold.co/82x37" />
+      <Avatar source="https://placehold.co/40x40" />
     </Container>
   );
 }
--- src/storage/meal/createMeal.ts
+++ src/storage/meal/createMeal.ts
@@ -1,5 +1,4 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import "react-native-get-random-values";
 import { v4 as uuidv4 } from "uuid";
 
 import { MEAL_COLLECTION } from "../storageConfig";
